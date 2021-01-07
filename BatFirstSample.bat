eaa073afa7aaa2b77b84fde3a7e9417fa38e34e7
govindrgraj <govindrgraj@gmail.com>
2021-01-07 11:15:50 +0530

    Webhook from Github

--- BatFirstSample.bat
+++ BatFirstSample.bat
@@ -1,5 +1,5 @@
 @echo OFF
 echo "Bat execution Start"
 echo "This is pull from Github repository"
-echo "After every PUSH The notification send to my jenkins and my targerted job Triggered - webhook"
+echo "After every PUSH The notification send to my jenkins and my targerted job Triggered - webhook enabled"
 echo "Bat execution End"
